f89f1de1008c6627f65f783eb9056eac4b6d32c3
jack-narv <jackricardonarvaezsalazar@gmail.com>
2021-10-17 18:01:05 -0500

    Corrección responsive

--- carrito/carrito.js
+++ carrito/carrito.js
@@ -57,16 +57,21 @@ function mostrar(){
 
 function mostrarMenu() {
     var elemento = document.getElementById("menuID");
+    var ul = document.getElementById("menublock");
     var espacio = document.getElementById("espacioMenu");
         if (!visible) {
              elemento.style.display = "block";
+             ul.style.display = "block";
              espacio.style.height = "395px";
              visible = true;
+
         } 
         else {
              elemento.style.display = "none";
+             ul.style.display = "none";
              espacio.style.height = "65px";
              visible = false;
+
         }
 } 
 
@@ -77,22 +82,21 @@ function sumar(){
     var sumatoria = 0;
     for(var i=0; i<precios.length; i++){
         var newWord=precios[i].innerText.slice(1,precios[i].innerText.length-1);
-       // console.log("PRECIO: "+parseFloat(newWord));
-       // console.log("CANTIDAD: "+parseFloat(cantidades[i].value));
         sumatoria += parseFloat(newWord) * parseFloat(cantidades[i].value)
     }
-    console.log("TOTAL A PAGAR: $"+sumatoria);
     total.innerHTML = "<div><p>$"+sumatoria+"</p></div>";
 }
 
- var mediaqueryList = window.matchMedia("(min-width: 820px)");
- mediaqueryList.addListener( function(EventoMediaQueryList) {
-        console.log('Ejecutado el listener', EventoMediaQueryList);
-        var elemento = document.getElementById("menuID");
-        var espacio = document.getElementById("espacioMenu");
-        elemento.style.display= "inline-block";
-        espacio.style.height = "65px";
-        });
+var mediaqueryList = window.matchMedia("(min-width: 857px)");
+mediaqueryList.addListener( function(EventoMediaQueryList) {
+       console.log('Ejecutado el listener', EventoMediaQueryList);
+       var elemento = document.getElementById("menuID");
+       var espacio = document.getElementById("espacioMenu");
+       var ul =document.getElementById("menublock");
+       elemento.style.display= "inline-block";
+       espacio.style.height = "65px";
+       ul.style.display = "block";
+       });
             
         window.addEventListener("load", iniciar);
         
--- getafe/getafe.js
+++ getafe/getafe.js
@@ -1,34 +1,43 @@
-
 var visible = false;
 
 function iniciar() {
     var elemento = document.getElementById("menu-img");
     elemento.addEventListener("click", mostrarMenu);
+    
 }
 
 function mostrarMenu() {
     var elemento = document.getElementById("menuID");
+    var ul = document.getElementById("menublock");
     var espacio = document.getElementById("espacioMenu");
         if (!visible) {
              elemento.style.display = "block";
+             ul.style.display = "block";
              espacio.style.height = "395px";
              visible = true;
+
         } 
         else {
              elemento.style.display = "none";
+             ul.style.display = "none";
              espacio.style.height = "65px";
              visible = false;
+
         }
 } 
 
- var mediaqueryList = window.matchMedia("(min-width: 820px)");
- mediaqueryList.addListener( function(EventoMediaQueryList) {
-        console.log('Ejecutado el listener', EventoMediaQueryList);
-        var elemento = document.getElementById("menuID");
-        var espacio = document.getElementById("espacioMenu");
-        elemento.style.display= "inline-block";
-        espacio.style.height = "65px";
-        });
+
+var mediaqueryList = window.matchMedia("(min-width: 857px)");
+mediaqueryList.addListener( function(EventoMediaQueryList) {
+       console.log('Ejecutado el listener', EventoMediaQueryList);
+       var elemento = document.getElementById("menuID");
+       var espacio = document.getElementById("espacioMenu");
+       var ul =document.getElementById("menublock");
+       elemento.style.display= "inline-block";
+       espacio.style.height = "65px";
+       ul.style.display = "block";
+       });
+
+        
             
         window.addEventListener("load", iniciar);
-        
